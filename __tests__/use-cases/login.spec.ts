a4e8aae127069b6f85e1b1c552dd5e2e7b449f7b
Glauber Gustavo <glaubergustavo70@gmail.com>
2024-08-09 10:09:34 -0300

    feat: add login use-case

--- __tests__/use-cases/login.spec.ts
+++ __tests__/use-cases/login.spec.ts
@@ -3,40 +3,9 @@ import { describe, it, expect } from "vitest"
 import { InMemoryUsersRepository } from "../repositories/in-memory-users"
 import { makeUser } from "../mock/entities/user"
 import { LoginError } from "../../src/use-cases/errors/login"
-import { UsersRepository } from "../../src/use-cases/interfaces/users-repository"
-import { JwtService } from "../../src/use-cases/interfaces/jwt"
 import { makeJwtService } from "../mock/services/jwt"
+import { LoginUseCase, LoginUseCaseReturn } from "../../src/use-cases/login"
 
-export interface LoginProps {
-    email: string
-    password: string
-}
-
-export interface LoginUseCaseReturn {
-    accessToken: string
-}
-
-export class LoginUseCase {
-    constructor(
-        private readonly userRepository: UsersRepository,
-        private readonly jwtService: JwtService
-    ) { }
-
-    async execute(props: LoginProps): Promise <LoginError | LoginUseCaseReturn> {
-        const userExist = await this.userRepository.findByEmail(props.email)
-
-        if (!userExist) {
-            return new LoginError("Invalid credentials!")
-        }
-        if (userExist.getPassword !== props.password) {
-            return new LoginError("Invalid credentials!")
-        }
-        const accessToken = await this.jwtService.createToken(userExist.getId)
-        return {
-            accessToken: accessToken
-        }
-    }
-}
 
 describe("LoginUser", () => {
     it("should not be able to login with user that does not exists", async () => {
---
+++ src/use-cases/login.ts
@@ -0,0 +1,34 @@
+import { LoginError } from "./errors/login"
+import { JwtService } from "./interfaces/jwt"
+import { UsersRepository } from "./interfaces/users-repository"
+
+export interface LoginProps {
+    email: string
+    password: string
+}
+
+export interface LoginUseCaseReturn {
+    accessToken: string
+}
+
+export class LoginUseCase {
+    constructor(
+        private readonly userRepository: UsersRepository,
+        private readonly jwtService: JwtService
+    ) { }
+
+    async execute(props: LoginProps): Promise <LoginError | LoginUseCaseReturn> {
+        const userExist = await this.userRepository.findByEmail(props.email)
+
+        if (!userExist) {
+            return new LoginError("Invalid credentials!")
+        }
+        if (userExist.getPassword !== props.password) {
+            return new LoginError("Invalid credentials!")
+        }
+        const accessToken = await this.jwtService.createToken(userExist.getId)
+        return {
+            accessToken: accessToken
+        }
+    }
+}
